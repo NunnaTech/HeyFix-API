db41999b9ba2b4792896639e59dcf0aeadde41fe
AlexisLoya <alexloy117@gmail.com>
2022-10-09 18:48:48 -0500

    categories functions completed

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
-const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// // Setup app dependencies
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express(); //create ExpressJS app
+
+
+// Automatically allow cross-origin requests
+const cors = require('cors');
+app.use(cors({ origin: true }));
+
+// Routes
+app.use(require('./routes/categories'));
+
+exports.app = functions.https.onRequest(app)
